0e9b8735a90485fccfb8d5f9c435feb4eeedcf3e
Richard Nemeth <ryshoooo@gmail.com>
2024-12-19 06:09:15 +0100

    fix: all

--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import (
 	"fmt"
 
+	no1 "github.com/ryshoooo/aoc2024/1"
+	no10 "github.com/ryshoooo/aoc2024/10"
+	no11 "github.com/ryshoooo/aoc2024/11"
+	no12 "github.com/ryshoooo/aoc2024/12"
+	no13 "github.com/ryshoooo/aoc2024/13"
 	no14 "github.com/ryshoooo/aoc2024/14"
+	no2 "github.com/ryshoooo/aoc2024/2"
+	no3 "github.com/ryshoooo/aoc2024/3"
+	no4 "github.com/ryshoooo/aoc2024/4"
+	no5 "github.com/ryshoooo/aoc2024/5"
+	no6 "github.com/ryshoooo/aoc2024/6"
+	no7 "github.com/ryshoooo/aoc2024/7"
+	no8 "github.com/ryshoooo/aoc2024/8"
+	no9 "github.com/ryshoooo/aoc2024/9"
 )
 
 func printPretty(no int, solver func() error) {
@@ -17,18 +30,18 @@ func printPretty(no int, solver func() error) {
 }
 
 func main() {
-	// printPretty(1, no1.Solve)
-	// printPretty(2, no2.Solve)
-	// printPretty(3, no3.Solve)
-	// printPretty(4, no4.Solve)
-	// printPretty(5, no5.Solve)
-	// printPretty(6, no6.Solve)
-	// printPretty(7, no7.Solve)
-	// printPretty(8, no8.Solve)
-	// printPretty(9, no9.Solve)
-	// printPretty(10, no10.Solve)
-	// printPretty(11, no11.Solve)
-	// printPretty(12, no12.Solve)
-	// printPretty(13, no13.Solve)
+	printPretty(1, no1.Solve)
+	printPretty(2, no2.Solve)
+	printPretty(3, no3.Solve)
+	printPretty(4, no4.Solve)
+	printPretty(5, no5.Solve)
+	printPretty(6, no6.Solve)
+	printPretty(7, no7.Solve)
+	printPretty(8, no8.Solve)
+	printPretty(9, no9.Solve)
+	printPretty(10, no10.Solve)
+	printPretty(11, no11.Solve)
+	printPretty(12, no12.Solve)
+	printPretty(13, no13.Solve)
 	printPretty(14, no14.Solve)
 }
